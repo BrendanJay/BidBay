52819d1a27f99ff549ddbaab58a1791001ff432b
Ramses C. Labora <ramzaa941@gmail.com>
2024-10-13 01:41:10 +1100

    [Seller-Dash][Ram]
    Adjusted the Seller's Dashboard

--- live-auction-app/src/Sellerpage/sellerhomepage/sellerhomepage.js
+++ live-auction-app/src/Sellerpage/sellerhomepage/sellerhomepage.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './sellerhomepage.css'; // Import CSS for styling
 
 function SellerHomePage() {
   const navigate = useNavigate();
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
 
   const handleAddNewItem = () => {
     navigate('/addnewitem');
@@ -22,28 +27,41 @@ function SellerHomePage() {
   };
 
   return (
-    <div className="App">
-    <div className="seller-homepage">
-      <header className="header">
-        <h2>Seller Dashboard</h2>
-      </header>
+       
+      
 
-      <div className="buttons-container">
-        <button className="dashboard-button" onClick={handleAddNewItem}>
-          Add New Item
-        </button>
-        <button className="dashboard-button" onClick={handleViewItems}>
-          View My Items
-        </button>
-        <button className="dashboard-button" onClick={handleViewSales}>
-          View Sales Analytics
-        </button>
-        <button className="dashboard-button logout" onClick={handleLogout}>
-          Logout
-        </button>
+    <div className="App">
+
+
+      
+     <div className="seller-homepage">
+     <div className="profile-container">
+      <div className="profile-image"></div>
+    
+    </div>
+      
+      <div className={`nav-toggle ${isNavOpen ? 'open' : ''}`} onClick={toggleNav}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      <nav className={`side-nav ${isNavOpen ? 'open' : ''}`}>
+        <button className="dashboard-button">Add new Item</button>
+        <button className="dashboard-button">View My Items</button>
+        <button className="dashboard-button">View Sales Analytics</button>
+        <button className="dashboard-button logout">Logout</button>
+      </nav>
+   
+      
+
+
+
     </div>
     </div>
+
+   
+
+
   );
 }
 
